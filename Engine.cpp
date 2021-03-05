cf05a7ea5813e7753bbbec92e7f31aa8cc6fedc3
경호 최 <rudgh710@naver.com>
2021-03-05 20:21:49 +0900

    test

--- Engine.cpp
+++ Engine.cpp
@@ -1,4 +1,7 @@
 #include "Engine.h"
+#include <stdio.h>
+#include <WinSock2.h>
+#include <assert.h>
 
 bool Engine::Initialize(HINSTANCE hInstance, std::string window_title, std::string window_class, int width, int height)
 {
@@ -7,6 +10,21 @@ bool Engine::Initialize(HINSTANCE hInstance, std::string window_title, std::stri
 
 	if (!gfx.Initialize(this->render_window.GetHWND(), width, height))
 		return false;
+
+	WSADATA wsaData;
+	SOCKET hSocket;
+	SOCKADDR_IN servAddr;
+
+	int strLen = 0;
+	int idx = 0;
+	int readLen = 0;
+
+	int fortNum = 17777;
+
+	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+	{
+		assert("")
+	}
 
 	return true;
 }
